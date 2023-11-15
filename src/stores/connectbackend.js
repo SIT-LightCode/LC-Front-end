e31576f45337be2b0307c08f344baa625a069431
Huaat <amornpong.@gmail.com>
2023-11-15 09:21:12 +0700

    fix headers

--- src/stores/connectbackend.js
+++ src/stores/connectbackend.js
@@ -8,13 +8,12 @@ export const connectbackend = defineStore("connectBackend", () => {
   const getAllTag = async () => {
     const res = await fetch(`${import.meta.env.VITE_BASE_URL}`, {
       method: "POST",
-      header: {
+      headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        "query":
-          "query GetTag { getTag { id\n    topic\n    description\n    lesson {\n      id\n      name\n      content\n    }\n  }\n}",
-        "operationName": "GetTag",
+        "query": "query GetTag { getTag { id\n    topic\n    description\n    lesson {\n      id\n      name\n      content\n    }\n  }\n}",
+        "operationName": "GetTag"
       }),
     });
     console.log(res.status);
