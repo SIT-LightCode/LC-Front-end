74e08a94f84872cc6a0cbee873e99dbc6b106b08
Tus <54474594+tuskingcup@users.noreply.github.com>
2023-12-02 22:08:06 +0700

    change vite base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const { VITE_BASE } = process.env;
+const VITE_BASE = process.env.VITE_BASE;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
