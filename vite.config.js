ed6e7a839c0b57b8f192a4894261740221e37d14
Tus <54474594+tuskingcup@users.noreply.github.com>
2023-12-09 00:00:47 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const VITE_BASE = process.env.VITE_BASE;
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: VITE_BASE,
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [
+      vue(),
+    ],
+    base: process.env.VITE_BASE,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-  ,
-  build: {
-    target: 'esnext', // Set the output format to "esm"
-  },
-})
+    ,
+    build: {
+      target: 'esnext', // Set the output format to "esm"
+    },
+  });
+}
