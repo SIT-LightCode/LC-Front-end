c6a044ad5bb8f8fb93963ce7e4916fa49a3533c3
markid1ggwp <markmopairat12345@gmail.com>
2024-02-08 05:52:17 +0700

    pull sp5

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,7 @@ const router = createRouter({
       path: '/view-user',
       name: 'viewuser',
       beforeEnter: checkLogin,
-      component: () => import('../Views/viewUser.vue'),
+      component: () => import('../views/viewUser.vue'),
     },
     {
       path: '/:pathMatch(.*)*',
