815be6dceb2ad6b686da63d6cac43d2d7e0632cb
Tus <54474594+tuskingcup@users.noreply.github.com>
2024-04-26 16:56:20 +0700

    new landing lightcode

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,16 +22,34 @@ module.exports = {
     extend: {
       keyframes: {
         wiggle: {
-            '0%, 100%': {
-                transform: 'rotate(-3deg)'
+          '0%, 100%': {
+              transform: 'rotate(-3deg)'
+          },
+          '50%': {
+              transform: 'rotate(3deg)'
+          },
+        },
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
             },
-            '50%': {
-                transform: 'rotate(3deg)'
+            "100%": {
+              width: "100%"
+            }
+          },
+          blink: {
+            "50%": {
+              borderColor: "transparent"
             },
+            "100%": {
+              borderColor: "white"
+            }
           }
         },
         animation: {
           wiggle: 'wiggle 1s ease-in-out infinite',
+          typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
         }
     },
     screens: {
